583a832677a783e0568da9a862b780d872a1bdb9
Traffic <trafficconegod@gmail.com>
2021-10-09 23:19:52 -0400

    fix segfaults from thread initialization

--- Source/Entities/Subclasses/Threads/Thread.cpp
+++ Source/Entities/Subclasses/Threads/Thread.cpp
@@ -9,44 +9,27 @@ static constexpr float Delta = 1.f/60.f;
 
 std::mutex clockMutex;
 
-Thread::Thread() : Entity() {
-	auto clock = std::chrono::high_resolution_clock::now();
+Thread::Thread() : worker{
+    [&]() {
+        auto clock = std::chrono::high_resolution_clock::now();
+        Start();
+        while (active) {
+            auto currentClock = std::chrono::high_resolution_clock::now();
 
-    std::exception_ptr exceptionPtr = nullptr;
-
-    worker = std::thread([&]() {
-        try {
-            Start();
-            while (active) {
-                clockMutex.lock();
-                auto currentClock = std::chrono::high_resolution_clock::now();
-                clockMutex.unlock();
-
-                std::chrono::duration<float> delta = currentClock - clock;
-                float deltaTime = delta.count();
-                if (delta.count() < Delta) {
-                    deltaTime = 1/60;
-                    float sleepTime = Delta - delta.count();
-                    std::this_thread::sleep_for(std::chrono::duration<float>(sleepTime));
-                }
-
-                clock = currentClock;
-                Update(deltaTime);
+            std::chrono::duration<float> delta = currentClock - clock;
+            float deltaTime = delta.count();
+            if (delta.count() < Delta) {
+                deltaTime = 1/60;
+                float sleepTime = Delta - delta.count();
+                std::this_thread::sleep_for(std::chrono::duration<float>(sleepTime));
             }
-            Stop();
-        } catch (...) {
-            exceptionPtr = std::current_exception();
-        }
-    });
 
-    if (exceptionPtr != nullptr) {
-        try {
-			std::rethrow_exception(exceptionPtr);
-		} catch(const std::exception& exception) {
-			std::cout << "Thread " << std::this_thread::get_id() << " caught exception from thread " << exception.what() << "\n";
-		}
+            Update(deltaTime);
+            clock = currentClock;
+        }
+        Stop();
     }
-}
+} {}
 
 Thread::~Thread() {
     if (!joined) {
